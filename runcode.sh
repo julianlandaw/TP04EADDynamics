bf12536b6638583e344372c27021d69046b0e817
Julian Landaw <julianlandaw@gmail.com>
2023-01-25 10:48:04 -0500

    correction and results folder

--- runcode.sh
+++ runcode.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-make BEATS=1500 REMOVEBEATS=2000 NUMVAR=9 NUMPCL=1 VARIABLE=nacafac
+make BEATS=2000 REMOVEBEATS=1500 NUMVAR=9 NUMPCL=1 VARIABLE=nacafac
 ./TTbif.out 1 5 2500 2500 0.05 
 ./TTap.out 1 2500 0.05 
 ./TTap.out 5 2500 0.05
+
+if [ ! -d "results" ] 
+then
+	mkdir results
+	mv TT*.txt results
+else
+	mv TT*.txt results
+fi
+	
